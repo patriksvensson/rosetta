406f2f3d77ff9129b9ede6bb87811cdb691410ff
Patrik Svensson <patrik@patriksvensson.se>
2022-07-20 09:46:38 +0200

    Fix compiler warnings

--- src/Rosetta/AssetFile.cs
+++ src/Rosetta/AssetFile.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using Rosetta.Internal;
 
 namespace Rosetta;
 
--- src/Rosetta/Properties/Usings.cs
+++ src/Rosetta/Properties/Usings.cs
@@ -2,7 +2,7 @@ global using System;
 global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.Linq;
-global using Rosetta.Internal;
 global using Newtonsoft.Json;
 global using NuGet.Versioning;
+global using Rosetta.Internal;
 global using Spectre.IO;
